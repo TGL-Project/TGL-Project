15379fd0a05168777576331c195252877c08cc70
AmatsukiUrato <AmatsukiUrato@yahoo.co.jp>
2016-11-19 14:25:42 +0900

    ファイル整理

--- Assets/Script/CsvManager.cs
+++ Assets/Script/CsvManager.cs
@@ -1,17 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
 using UnityEngine;
 using System.Collections;
-using System.IO;
 
-public class CsvManager {
+public class CsvManager : MonoBehaviour {
+	private string[] timeTable=null;
+	// Use this for initialization
+	void Start()
+	{
+		ReadCsv ();
 
-    static void ReadCsv() {
-        TextAsset csv = Resources.Load("CsvData") as TextAsset;
-        StringReader reader = new StringReader(csv.text);
-        while (reader.Peek() > -1)
-        {
-            string line = reader.ReadLine();
-            string[] values = line.Split(',');
-            Debug.Log(values);
-        }
-    }
+		NextTime (DateTime.Now);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	static void ReadCsv()
+	{
+		string filePath = "TimeTable.csv";
+		StreamReader reader = new StreamReader(filePath);
+		while (reader.Peek() >= 0) {
+			string[] cols = reader.ReadLine().Split(',');
+			for (int n = 0; n < cols.Length; n++) {
+				
+
+			}
+
+
+
+		}
+		reader.Close();
+	}
+
+
+
+
+	void NextTime(DateTime time)
+	{ 
+		for(int n = 0; n < timeTable.Length;n++){
+			DateTime timeTable = DateTime.Parse(timeTable [n]);
+
+			Debug.Log(timeTable);
+
+
+		}
+	}
+
 }
