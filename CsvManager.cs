216daf7bd9a4c28ca947534ebc18c17ba454dd3c
AmatsukiUrato <AmatsukiUrato@yahoo.co.jp>
2016-11-19 14:12:34 +0900

    CSVファイル，CSV読み込みプログラムの追加

---
+++ CsvManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+public class CsvManager : MonoBehaviour {
+	private string[] timeTable=null;
+	// Use this for initialization
+	void Start()
+	{
+		ReadCsv ();
+
+		NextTime (DateTime.Now);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	static void ReadCsv()
+	{
+		string filePath = "TimeTable.csv";
+		StreamReader reader = new StreamReader(filePath);
+		while (reader.Peek() >= 0) {
+			string[] cols = reader.ReadLine().Split(',');
+			for (int n = 0; n < cols.Length; n++) {
+				
+
+			}
+
+
+
+		}
+		reader.Close();
+	}
+
+
+
+
+	void NextTime(DateTime time)
+	{ 
+		for(int n = 0; n < timeTable.Length;n++){
+			DateTime timeTable = DateTime.Parse(timeTable [n]);
+
+			Debug.Log(timeTable);
+
+
+		}
+	}
+
+}
